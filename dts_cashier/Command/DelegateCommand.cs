79b8448b3b334ca768c7310441d677bfa5a88452
SangDoThanh <sangdothanh95@gmail.com>
2024-11-10 10:47:46 +0700

    xong màn hình MainWindow

---
+++ dts_cashier/Command/DelegateCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace dts_cashier.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action action;
+        private Action<string> gotFocus;
+        private DelegateCommand openDropDown;
+
+        public DelegateCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public DelegateCommand(DelegateCommand openDropDown)
+        {
+            this.openDropDown = openDropDown;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> action;
+
+        public DelegateCommand(Action<T> action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            action((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+    }
+}
