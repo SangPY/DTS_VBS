11d9f7a4a66da953af0db73d63dd480bc318a301
SangDoThanh <sangdothanh95@gmail.com>
2024-11-17 11:17:28 +0700

    Thêm lấy thông tin resource từ new_dts_lite_resource

--- dts_agent/Helper/IViewService.cs
+++ dts_agent/Helper/IViewService.cs
@@ -4,31 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using dts_agent.Components.Base;
+using static dts_agent.Helper.ViewService;
 
 namespace dts_agent.Helper
 {
     public interface IViewService
     {
-        ///// <summary>
-        ///// Use this to show views whose view models do not have a parameter.
-        ///// </summary>
-        //void ShowView<TViewModel>() where TViewModel : BaseViewModel;
+        /// <summary>
+        /// Use this to show views whose view models do not have a parameter.
+        /// </summary>
+        void ShowView<TViewModel>() where TViewModel : BaseViewModel;
 
-        ///// <summary>
-        ///// Use this to show views whose view models do not have a parameter.
-        ///// </summary>
-        //void ShowDialog<TViewModel>(Window owner) where TViewModel : BaseViewModel;
+        /// <summary>
+        /// Use this to show views whose view models do not have a parameter.
+        /// </summary>
+        void ShowDialog<TViewModel>(Window owner) where TViewModel : BaseViewModel;
 
-        ///// <summary>
-        ///// Use this to show views whose view models contain parameters.
-        ///// The NewViewModelDelegate is responsible for returning the new instance of the view model given the view.
-        ///// </summary>
-        //void ShowView<TViewModel>(ViewService.NewViewModelDelegate nvmDelegate) where TViewModel : BaseViewModel;
+        /// <summary>
+        /// Use this to show views whose view models contain parameters.
+        /// The NewViewModelDelegate is responsible for returning the new instance of the view model given the view.
+        /// </summary>
+        void ShowView<TViewModel>(ViewService.NewViewModelDelegate nvmDelegate) where TViewModel : BaseViewModel;
 
-        ///// <summary>
-        ///// Use this to show views whose view models contain parameters.
-        ///// The NewViewModelDelegate is responsible for returning the new instance of the view model given the view.
-        ///// </summary>
-        //void ShowDialog<TViewModel>(ViewService.NewViewModelDelegate nvmDelegate, Window owner) where TViewModel : BaseViewModel;
+        /// <summary>
+        /// Use this to show views whose view models contain parameters.
+        /// The NewViewModelDelegate is responsible for returning the new instance of the view model given the view.
+        /// </summary>
+        void ShowDialog<TViewModel>(ViewService.NewViewModelDelegate nvmDelegate, Window owner) where TViewModel : BaseViewModel;
     }
 }
--- dts_agent/Helper/ViewService.cs
+++ dts_agent/Helper/ViewService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using dts_agent.Components.Base;
 using dts_agent.Components.Login;
 
@@ -17,7 +18,7 @@ namespace dts_agent.Helper
         {
             Mappings = new Dictionary<Type, Type>();
             //Register<DashboardViewModel, DashboardView>();
-            //Register<LoginViewModel, LoginView>();
+            Register<LoginViewModel, LoginWiew>();
         }
 
         public static ViewService Instance
@@ -30,13 +31,26 @@ namespace dts_agent.Helper
 
         public delegate BaseViewModel NewViewModelDelegate(IView view);
 
-        public void ShowView<TViewModel>() where TViewModel : BaseViewModel
+        private void Register<TViewModel, TView>() where TViewModel : BaseViewModel
+                                                  where TView : IView
         {
-            if (Mappings == null)
+            if (Mappings.ContainsKey(typeof(TViewModel)))
             {
-                throw new InvalidOperationException("Mappings chưa được khởi tạo.");
+                throw new ArgumentException($"Type {typeof(TViewModel)} is already mapped to type {typeof(TView)}");
             }
 
+            Mappings.Add(typeof(TViewModel), typeof(TView));
+        }
+
+        public void ShowView<TViewModel>() where TViewModel : BaseViewModel
+        {
+            // chưa khai báo  Mappings = new Dictionary<Type, Type>();
+            //if (Mappings == null)
+            //{
+            //    throw new InvalidOperationException("Mappings chưa được khởi tạo.");
+            //}
+
+            // chưa đăng ký Register<LoginViewModel, LoginWiew>();
             //if (!Mappings.TryGetValue(typeof(TViewModel), out Type viewType))
             //{
             //    throw new KeyNotFoundException($"Không tìm thấy key cho ViewModel {typeof(TViewModel).Name} trong Mappings.");
@@ -46,6 +60,36 @@ namespace dts_agent.Helper
             IView view = (IView)Activator.CreateInstance(viewType);
             view.DataContext = (TViewModel)Activator.CreateInstance(typeof(TViewModel), new object[] { view });
             view.Show();
+        }
+
+        public void ShowView<TViewModel>(NewViewModelDelegate nvmDelegate) where TViewModel : BaseViewModel
+        {
+            Type viewType = Mappings[typeof(TViewModel)];
+            IView view = (IView)Activator.CreateInstance(viewType);
+            view.DataContext = nvmDelegate(view);
+            view.Show();
+        }
+
+        public void ShowDialog<TViewModel>(Window owner) where TViewModel : BaseViewModel
+        {
+            Type viewType = Mappings[typeof(TViewModel)];
+
+            IView view = (IView)Activator.CreateInstance(viewType);
+
+            view.DataContext = (TViewModel)Activator.CreateInstance(typeof(TViewModel), new object[] { view });
+            view.Owner = owner;
+            view.ShowDialog();
+        }
+
+        public void ShowDialog<TViewModel>(NewViewModelDelegate nvmDelegate, Window owner) where TViewModel : BaseViewModel
+        {
+            Type viewType = Mappings[typeof(TViewModel)];
+
+            IView view = (IView)Activator.CreateInstance(viewType);
+
+            view.DataContext = nvmDelegate(view);
+            view.Owner = owner;
+            view.ShowDialog();
         }
     }
 }
